6d44c094d8d348f69cda5fbf18437d1664e86896
gavin-guo <igavin.guo@gmail.com>
2017-04-04 08:42:10 +0800

    commit.

---
+++ common/src/main/java/com/gavin/CommonApplication.java
@@ -0,0 +1,12 @@
+package com.gavin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonApplication.class, args);
+	}
+}
