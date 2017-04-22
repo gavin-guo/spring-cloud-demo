2c39b0e1b299f0dfabd6e8400e1cbb7e2d312d77
gavin-guo <gavin.guo@msn.com>
2017-04-22 21:30:20 +0800

    resolve the problem that resource server does not work.

--- zuul-service/src/main/java/com/gavin/config/ResourceServerConfiguration.java
+++ zuul-service/src/main/java/com/gavin/config/ResourceServerConfiguration.java
@@ -1,42 +1,15 @@
 package com.gavin.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
-//@Configuration
+@Configuration
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
-//    private final ResourceServerProperties resourceServerProperties;
-//
-//    private final UserDetailsService userDetailsService;
-//
-//    @Autowired
-//    public ResourceServerConfiguration(
-//            ResourceServerProperties resourceServerProperties,
-//            @Qualifier("customUserDetailsService") UserDetailsService userDetailsService) {
-//        this.resourceServerProperties = resourceServerProperties;
-//        this.userDetailsService = userDetailsService;
-//    }
-//
-//    @Override
-//    public void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests().anyRequest().authenticated();
-//    }
-//
-//    @Override
-//    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-//        DefaultUserAuthenticationConverter userAuthenticationConverter = new DefaultUserAuthenticationConverter();
-//        userAuthenticationConverter.setUserDetailsService(userDetailsService);
-//
-//        DefaultAccessTokenConverter accessTokenConverter = new DefaultAccessTokenConverter();
-//        accessTokenConverter.setUserTokenConverter(userAuthenticationConverter);
-//
-//        RemoteTokenServices remoteTokenServices = new RemoteTokenServices();
-//        remoteTokenServices.setAccessTokenConverter(accessTokenConverter);
-//        remoteTokenServices.setCheckTokenEndpointUrl(resourceServerProperties.getTokenInfoUri());
-//        remoteTokenServices.setClientId(resourceServerProperties.getClientId());
-//        remoteTokenServices.setClientSecret(resourceServerProperties.getClientSecret());
-//
-//        resources.tokenServices(remoteTokenServices);
-//    }
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated();
+    }
 
 }
