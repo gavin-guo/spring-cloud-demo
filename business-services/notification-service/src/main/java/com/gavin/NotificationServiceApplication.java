b1c279516765ab37d62043eea1b90172a34b138c
gavin-guo <gavin.guo@msn.com>
2017-05-04 22:36:47 +0800

    modify notification-service.

--- business-services/notification-service/src/main/java/com/gavin/NotificationServiceApplication.java
+++ business-services/notification-service/src/main/java/com/gavin/NotificationServiceApplication.java
@@ -1,12 +1,15 @@
 package com.gavin;
 
+import com.gavin.messaging.UserCreatedProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.retry.annotation.EnableRetry;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableRetry
+@EnableBinding(UserCreatedProcessor.class)
 @EnableSwagger2
 public class NotificationServiceApplication {
 
