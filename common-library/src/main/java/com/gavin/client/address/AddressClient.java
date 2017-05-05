4a235cce621a0ef341130704d1fae14a8d40e2e7
gavin-guo <gavin.guo@msn.com>
2017-05-06 00:39:00 +0800

    modify AddressClient.

--- common-library/src/main/java/com/gavin/client/address/AddressClient.java
+++ common-library/src/main/java/com/gavin/client/address/AddressClient.java
@@ -3,7 +3,6 @@ package com.gavin.client.address;
 import com.gavin.model.CustomResponseBody;
 import com.gavin.model.dto.address.AddressDto;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient(value = "address-service", fallback = AddressClientFallback.class)
 public interface AddressClient {
 
-    @RequestMapping(value = "/addresses/{address_id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/addresses/{address_id}", method = RequestMethod.GET)
     CustomResponseBody<AddressDto> findAddressById(@PathVariable("address_id") String _addressId);
 
 }
