dd5bd219b657d970441753e6bcf6fff1238efb97
gavin-guo <gavin.guo@msn.com>
2017-05-06 08:06:58 +0800

    change module name: eureka-service -> eureka-server.

--- eureka-server/src/main/java/com/gavin/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/gavin/EurekaServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServiceApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServiceApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
--- eureka-server/src/test/java/com/gavin/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/gavin/EurekaServerApplicationTests.java
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaServiceApplicationTests {
+public class EurekaServerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
