cf2fc5af70ae05f8db7c1bcd52d7f6004cb432eb
gavin-guo <gavin.guo@msn.com>
2017-05-06 08:11:14 +0800

    change module name: config-service -> config-server.

--- config-server/src/main/java/com/gavin/ConfigServerApplication.java
+++ config-server/src/main/java/com/gavin/ConfigServerApplication.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableConfigServer
-public class ConfigServiceApplication {
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServiceApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 
 }
--- config-server/src/test/java/com/gavin/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/gavin/ConfigServerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ConfigServiceApplicationTests {
+public class ConfigServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
