5918782d90dcb617e693414a74f190c87aac1aa2
gavin-guo <gavin.guo@msn.com>
2017-05-08 14:30:20 +0800

    fix utf8 problem.

--- scripts/start-docker-containers.sh
+++ scripts/start-docker-containers.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+cd docker-compose
+pwd
 docker-compose up -d
+docker ps
--- scripts/stop-docker-containers.sh
+++ scripts/stop-docker-containers.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+cd docker-compose
+pwd
 docker-compose stop
 docker-compose rm -f
+docker ps
