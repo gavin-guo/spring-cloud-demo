3e52d1b846d9fc6fbe0f39c0f04b5799ca3d9424
gavin-guo <gavin.guo@msn.com>
2017-05-10 13:57:57 +0800

    modify AddLoginUserHeaderFilter.

--- zuul-server/src/main/java/com/gavin/filter/AddLoginUserHeaderFilter.java
+++ zuul-server/src/main/java/com/gavin/filter/AddLoginUserHeaderFilter.java
@@ -5,18 +5,15 @@ import com.netflix.zuul.context.RequestContext;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.codec.Base64;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.stereotype.Component;
 
-import java.io.UnsupportedEncodingException;
-
 @Component
 @Slf4j
-public class AddLoginInfoHeaderFilter extends ZuulFilter {
+public class AddLoginUserHeaderFilter extends ZuulFilter {
 
-    private static final String LOGIN_INFO = "LOGIN_INFO";
+    private static final String LOGIN_USER_ID = "LOGIN_USER_ID";
 
     @Override
     public String filterType() {
@@ -38,20 +35,19 @@ public class AddLoginInfoHeaderFilter extends ZuulFilter {
                 if (details instanceof OAuth2AuthenticationDetails) {
                     OAuth2AuthenticationDetails authenticationDetails = (OAuth2AuthenticationDetails) details;
 
-                    String loginUser = (String) authentication.getPrincipal();
-                    String token = authenticationDetails.getTokenValue();
+                    String userId = (String) authentication.getPrincipal();
 
-                    String loginInfo = String.format("%s:%s", loginUser, token);
-
-                    String encoded;
-                    try {
-                        encoded = new String(Base64.encode(loginInfo.getBytes("UTF-8")));
-                    } catch (UnsupportedEncodingException e) {
-                        throw new IllegalStateException("Could not convert String");
-                    }
+//                    String token = authenticationDetails.getTokenValue();
+//                    String loginInfo = String.format("%s:%s", loginUser, token);
+//                    String encoded;
+//                    try {
+//                        encoded = new String(Base64.encode(loginInfo.getBytes("UTF-8")));
+//                    } catch (UnsupportedEncodingException e) {
+//                        throw new IllegalStateException("Could not convert String");
+//                    }
 
                     RequestContext ctx = RequestContext.getCurrentContext();
-                    ctx.set(LOGIN_INFO, encoded);
+                    ctx.set(LOGIN_USER_ID, userId);
                     return true;
                 }
             }
@@ -65,7 +61,7 @@ public class AddLoginInfoHeaderFilter extends ZuulFilter {
     @Override
     public Object run() {
         RequestContext ctx = RequestContext.getCurrentContext();
-        ctx.addZuulRequestHeader("login-info", (String) ctx.get(LOGIN_INFO));
+        ctx.addZuulRequestHeader("x-user-id", (String) ctx.get(LOGIN_USER_ID));
         return null;
     }
 
