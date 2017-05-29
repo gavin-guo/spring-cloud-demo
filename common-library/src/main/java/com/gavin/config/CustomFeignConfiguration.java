d3dbde436bbe734f068701eb32dea97f841d9670
gavin-guo <gavin.guo@msn.com>
2017-05-29 17:11:11 +0800

    remove customRequestInterceptor because it can not work when hystrix strategy is THREAD.

--- common-library/src/main/java/com/gavin/config/CustomFeignConfiguration.java
+++ common-library/src/main/java/com/gavin/config/CustomFeignConfiguration.java
@@ -1,14 +1,9 @@
 package com.gavin.config;
 
-import com.gavin.constants.RequestAttributeConstants;
-import com.gavin.model.dto.security.CurrentUser;
 import feign.Logger;
-import feign.RequestInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
 
 @Configuration
 @Slf4j
@@ -19,17 +14,17 @@ public class CustomFeignConfiguration {
         return Logger.Level.FULL;
     }
 
-    @Bean
-    public RequestInterceptor customRequestInterceptor() {
-        return template -> {
-            RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-
-            Object obj = requestAttributes.getAttribute(RequestAttributeConstants.CURRENT_USER, RequestAttributes.SCOPE_REQUEST);
-            if (obj != null) {
-                CurrentUser currentUser = (CurrentUser) obj;
-                template.header("x-user-id", currentUser.getUserId());
-            }
-        };
-    }
+//    @Bean
+//    public RequestInterceptor customRequestInterceptor() {
+//        return template -> {
+//            RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+//
+//            Object obj = requestAttributes.getAttribute(RequestAttributeConstants.CURRENT_USER, RequestAttributes.SCOPE_REQUEST);
+//            if (obj != null) {
+//                CurrentUser currentUser = (CurrentUser) obj;
+//                template.header("x-user-id", currentUser.getUserId());
+//            }
+//        };
+//    }
 
 }
