ffcb8cdc18a87356a5afc1558ed494c865b25c87
gavin-guo <gavin.guo@msn.com>
2017-06-04 10:13:12 +0800

    add kafka launch setting.

---
+++ scripts/start-kafka.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+zkServer start
+sh /usr/local/bin/kafka-server-start /usr/local/etc/kafka/server.properties
---
+++ scripts/stop-kafka.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sh /usr/local/bin/kafka-server-stop
+zkServer stop
