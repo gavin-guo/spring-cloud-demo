449e5e40b4432be11d58883693782638b058ac13
gavin-guo <gavin.guo@msn.com>
2017-06-10 16:33:50 +0800

    no message

--- business-services/address-service/src/main/java/com/gavin/AddressServiceApplication.java
+++ business-services/address-service/src/main/java/com/gavin/AddressServiceApplication.java
@@ -2,13 +2,11 @@ package com.gavin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableCircuitBreaker
 @EnableSwagger2
 public class AddressServiceApplication {
 
