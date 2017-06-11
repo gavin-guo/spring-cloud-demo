0072c6b553bdd12fafd8063a6d4cd23fc77e4dac
gavin-guo <gavin.guo@msn.com>
2017-06-11 21:03:41 +0800

    no message

--- scripts/start-kafka.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-zkServer start
-sh /usr/local/bin/kafka-server-start /usr/local/etc/kafka/server.properties
--- scripts/stop-kafka.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-sh /usr/local/bin/kafka-server-stop
-zkServer stop
