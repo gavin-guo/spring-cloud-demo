b6edd651f34d64afcfffed1317cb78dd47ded735
gavin-guo <gavin.guo@msn.com>
2017-06-18 09:48:12 +0800

    change package and classes name.

---
+++ business-services/product-service/src/main/java/com/gavin/repository/elasticsearch/ProductElasticsearchRepository.java
@@ -0,0 +1,9 @@
+//package com.gavin.repository.elasticsearch;
+//
+//import com.gavin.domain.Product;
+//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface ProductElasticsearchRepository extends ElasticsearchRepository<Product, String> {
+//}
