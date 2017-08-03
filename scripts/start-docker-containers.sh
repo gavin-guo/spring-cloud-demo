2feaad8d73fe8c4a8a9b14e7cb8119ed6d1a9073
gavin-guo <gavin.guo@msn.com>
2017-08-03 16:28:44 +0800

    remove docker-compose.

--- scripts/start-docker-containers.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd docker-compose
-pwd
-docker-compose up -d
-docker ps
--- scripts/stop-docker-containers.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd docker-compose
-pwd
-docker-compose stop
-docker-compose rm -f
-docker ps
