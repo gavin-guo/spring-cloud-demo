be3d3bc10fefed59614eb13e93e01b2100cad62d
gavin-guo <gavin.guo@msn.com>
2017-09-20 15:34:48 +0800

    modify oauth-server to adapt to k8s.

--- oauth2-server/src/main/resources/db/changelog/changelog-0.10.sql
+++ oauth2-server/src/main/resources/db/changelog/changelog-0.10.sql
@@ -1,72 +1,72 @@
 --liquibase formatted SQL
 
 --changeset gavin:1
-DROP TABLE IF EXISTS `OAUTH_ACCESS_TOKEN`;
-CREATE TABLE `OAUTH_ACCESS_TOKEN` (
-  `TOKEN_ID`          VARCHAR(256) DEFAULT NULL,
-  `TOKEN`             BLOB,
-  `AUTHENTICATION_ID` VARCHAR(256) DEFAULT NULL,
-  `USER_NAME`         VARCHAR(256) DEFAULT NULL,
-  `CLIENT_ID`         VARCHAR(256) DEFAULT NULL,
-  `AUTHENTICATION`    BLOB,
-  `REFRESH_TOKEN`     VARCHAR(256) DEFAULT NULL
+DROP TABLE IF EXISTS `oauth_access_token`;
+CREATE TABLE `oauth_access_token` (
+  `token_id`          VARCHAR(256) DEFAULT NULL,
+  `token`             BLOB,
+  `authentication_id` VARCHAR(256) DEFAULT NULL,
+  `user_name`         VARCHAR(256) DEFAULT NULL,
+  `client_id`         VARCHAR(256) DEFAULT NULL,
+  `authentication`    BLOB,
+  `refresh_token`     VARCHAR(256) DEFAULT NULL
 )
-  ENGINE = InnoDB
+  ENGINE = innodb
   DEFAULT CHARSET = utf8;
 
-DROP TABLE IF EXISTS `OAUTH_REFRESH_TOKEN`;
-CREATE TABLE `OAUTH_REFRESH_TOKEN` (
-  `TOKEN_ID`       VARCHAR(256) DEFAULT NULL,
-  `TOKEN`          BLOB,
-  `AUTHENTICATION` BLOB
+DROP TABLE IF EXISTS `oauth_refresh_token`;
+CREATE TABLE `oauth_refresh_token` (
+  `token_id`       VARCHAR(256) DEFAULT NULL,
+  `token`          BLOB,
+  `authentication` BLOB
 )
-  ENGINE = InnoDB
+  ENGINE = innodb
   DEFAULT CHARSET = utf8;
 
-DROP TABLE IF EXISTS `OAUTH_APPROVALS`;
-CREATE TABLE `OAUTH_APPROVALS` (
-  `USERID`         VARCHAR(256)   DEFAULT NULL,
-  `CLIENTID`       VARCHAR(256)   DEFAULT NULL,
-  `SCOPE`          VARCHAR(256)   DEFAULT NULL,
-  `STATUS`         VARCHAR(10)    DEFAULT NULL,
-  `EXPIRESAT`      TIMESTAMP NULL DEFAULT NULL,
-  `LASTMODIFIEDAT` TIMESTAMP NULL DEFAULT NULL
+DROP TABLE IF EXISTS `oauth_approvals`;
+CREATE TABLE `oauth_approvals` (
+  `userid`         VARCHAR(256)   DEFAULT NULL,
+  `clientid`       VARCHAR(256)   DEFAULT NULL,
+  `scope`          VARCHAR(256)   DEFAULT NULL,
+  `status`         VARCHAR(10)    DEFAULT NULL,
+  `expiresat`      TIMESTAMP NULL DEFAULT NULL,
+  `lastmodifiedat` TIMESTAMP NULL DEFAULT NULL
 )
-  ENGINE = InnoDB
+  ENGINE = innodb
   DEFAULT CHARSET = utf8;
 
-DROP TABLE IF EXISTS `OAUTH_CODE`;
-CREATE TABLE `OAUTH_CODE` (
-  `CODE`           VARCHAR(256) DEFAULT NULL,
-  `AUTHENTICATION` BLOB
+DROP TABLE IF EXISTS `oauth_code`;
+CREATE TABLE `oauth_code` (
+  `code`           VARCHAR(256) DEFAULT NULL,
+  `authentication` BLOB
 )
-  ENGINE = InnoDB
+  ENGINE = innodb
   DEFAULT CHARSET = utf8;
 
-DROP TABLE IF EXISTS `OAUTH_CLIENT_DETAILS`;
-CREATE TABLE `OAUTH_CLIENT_DETAILS` (
-  `CLIENT_ID`               VARCHAR(255) NOT NULL,
-  `RESOURCE_IDS`            VARCHAR(256)  DEFAULT NULL,
-  `CLIENT_SECRET`           VARCHAR(256)  DEFAULT NULL,
-  `SCOPE`                   VARCHAR(256)  DEFAULT NULL,
-  `AUTHORIZED_GRANT_TYPES`  VARCHAR(256)  DEFAULT NULL,
-  `WEB_SERVER_REDIRECT_URI` VARCHAR(256)  DEFAULT NULL,
-  `AUTHORITIES`             VARCHAR(256)  DEFAULT NULL,
-  `ACCESS_TOKEN_VALIDITY`   INT(11)       DEFAULT NULL,
-  `REFRESH_TOKEN_VALIDITY`  INT(11)       DEFAULT NULL,
-  `ADDITIONAL_INFORMATION`  VARCHAR(4096) DEFAULT NULL,
-  `AUTOAPPROVE`             VARCHAR(45)   DEFAULT 'true',
-  PRIMARY KEY (`CLIENT_ID`)
+DROP TABLE IF EXISTS `oauth_client_details`;
+CREATE TABLE `oauth_client_details` (
+  `client_id`               VARCHAR(255) NOT NULL,
+  `resource_ids`            VARCHAR(256)  DEFAULT NULL,
+  `client_secret`           VARCHAR(256)  DEFAULT NULL,
+  `scope`                   VARCHAR(256)  DEFAULT NULL,
+  `authorized_grant_types`  VARCHAR(256)  DEFAULT NULL,
+  `web_server_redirect_uri` VARCHAR(256)  DEFAULT NULL,
+  `authorities`             VARCHAR(256)  DEFAULT NULL,
+  `access_token_validity`   INT(11)       DEFAULT NULL,
+  `refresh_token_validity`  INT(11)       DEFAULT NULL,
+  `additional_information`  VARCHAR(4096) DEFAULT NULL,
+  `autoapprove`             VARCHAR(45)   DEFAULT 'true',
+  PRIMARY KEY (`client_id`)
 )
-  ENGINE = InnoDB
+  ENGINE = innodb
   DEFAULT CHARSET = utf8;
 
 --changeset gavin:2
-INSERT INTO OAUTH_CLIENT_DETAILS (CLIENT_ID, RESOURCE_IDS, CLIENT_SECRET, SCOPE, AUTHORIZED_GRANT_TYPES, WEB_SERVER_REDIRECT_URI, AUTHORITIES, ACCESS_TOKEN_VALIDITY, REFRESH_TOKEN_VALIDITY, ADDITIONAL_INFORMATION, AUTOAPPROVE)
+INSERT INTO oauth_client_details (client_id, resource_ids, client_secret, scope, authorized_grant_types, web_server_redirect_uri, authorities, access_token_validity, refresh_token_validity, additional_information, autoapprove)
 VALUES
 ('ui', NULL, 'secret', 'ui-scope', 'authorization_code,password,refresh_token,client_credentials', '',
-'ROLE_CLIENT, ROLE_TRUSTED_CLIENT', '30000', '30000', NULL, 'false');
-INSERT INTO OAUTH_CLIENT_DETAILS (CLIENT_ID, RESOURCE_IDS, CLIENT_SECRET, SCOPE, AUTHORIZED_GRANT_TYPES, WEB_SERVER_REDIRECT_URI, AUTHORITIES, ACCESS_TOKEN_VALIDITY, REFRESH_TOKEN_VALIDITY, ADDITIONAL_INFORMATION, AUTOAPPROVE)
+'role_client, role_trusted_client', '30000', '30000', NULL, 'false');
+INSERT INTO oauth_client_details (client_id, resource_ids, client_secret, scope, authorized_grant_types, web_server_redirect_uri, authorities, access_token_validity, refresh_token_validity, additional_information, autoapprove)
 VALUES
   ('zuul', NULL, 'secret', 'server-scope', 'client_credentials', '',
-           'ROLE_CLIENT, ROLE_TRUSTED_CLIENT', '30000', '30000', NULL, 'false');
+           'role_client, role_trusted_client', '30000', '30000', NULL, 'false');
