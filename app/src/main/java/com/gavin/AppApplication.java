4b15b56720823e786b53e2823ef9f10fbabed000
gavin-guo <gavin.guo@msn.com>
2017-11-25 21:08:44 +0800

    add front service: app & api.

---
+++ app/src/main/java/com/gavin/AppApplication.java
@@ -0,0 +1,12 @@
+package com.gavin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+}
