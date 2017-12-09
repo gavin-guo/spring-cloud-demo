79ef279729848efa1dac9c24989752bd190359a4
gavin-guo <gavin.guo@msn.com>
2017-12-09 17:31:37 +0800

    add tenant service.

---
+++ business/tenant/src/main/java/com/gavin/business/TenantApplication.java
@@ -0,0 +1,12 @@
+package com.gavin.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenantApplication.class, args);
+	}
+}
